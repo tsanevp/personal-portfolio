55760b597f4a4722263e10aba5b7271124fef47a
tsanevp <petertsanev7@gmail.com>
2025-01-22 23:08:11 -0800

    added imports

--- app.vue
+++ app.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 import type { Api } from "~/types/api";
+
 const experienceStore = useExperienceStore();
 const projectStore = useProjectStore();
 const stackStore = useStackStore();
